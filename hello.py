a0900d3492842afc53384b907c105d61d45a4d64
HarshPatel1521 <harshpate.10501@gmail.com>
2023-10-01 15:50:44 +0100

    Inital Commit

---
+++ hello.py
@@ -0,0 +1,69 @@
+# import time
+# import io
+
+print("Hello Python")
+
+# seconds = int(input("Enter a number: "))
+# print("Here we go")
+
+# TIME
+# for i in reversed(range(seconds + 1)):
+#     if i > 0:
+#         print(i)
+#         time.sleep(1)
+#     else:
+#         print('Let it begin')
+
+#IO
+# with open('text.txt') as f:
+#     print(f.read())
+
+# Arithmetic operators
+# x = 3
+# y = 7
+
+# add = x+y
+# sub =  x-y
+# mul = x*y
+# div = x/y
+# divi = x//y
+# modulo = x%y
+# pow = x**y
+
+# print("Addition:",add)
+# print("Subtraction:",sub)
+# print("Multiplication:",mul)
+# print("Division:",div)
+# print("Division (Floor):",divi)
+# print("Modulo:",modulo)
+# print("Power:",pow)
+
+# print("x>y", x>y)
+# print("x<y", x<y)
+# print("x==y", x==y)
+# print("x!=y", x!=y)
+# print("x>=y", x>=y)
+# print("x<=y", x<=y)
+
+# Strings
+# string1 = input("Enter an String: ")
+
+# Reversing
+# string2 = string1[::-1]
+# print(string1, "|", string2)
+
+# Split String
+# x = int(input("Enter first Number: "))
+# y = int(input("Enter second Number: "))
+# print(string1[x:y])
+
+# Lists
+list = [10, 20, 30]
+print("Numbers list: ", list)
+
+list1 = ["Patel", "harsh", "Voldemort"]
+print("String Lists: ", list1)
+list1[2] = "Kalpeshbhai"
+print("New List1: ", list1)
+list2 = list1.append(list)
+print(list2)
